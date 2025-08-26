c9e80dd5cf879033d6171f2225bbc1798242e3e8
Mareike Heuer <mareike.heuer@hotmail.com>
2025-08-26 22:55:13 +0200

    Linking a JavaScript file

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,6 @@
+/* JS code in its own file --> Better separation of concern between presentation and logic */
+
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN!');
+
+console.log(40 + 8 + 23 - 10);
