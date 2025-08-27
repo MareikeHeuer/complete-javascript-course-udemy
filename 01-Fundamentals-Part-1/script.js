3b257fccf4c28abe63bb3ce208577e3ced08a351
Mareike Heuer <mareike.heuer@hotmail.com>
2025-08-27 23:34:00 +0200

    Section 2: Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,6 +1,6 @@
 /* JS code in its own file --> Better separation of concern between presentation and logic */
 
-let js = 'amazing';
+/* let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 //// VALUES ////
@@ -17,7 +17,7 @@ let firstNamePerson;
 console.log(firstName);
 console.log(firstName);
 
-/* 
+
 Conventions for variable names
 
 - camelCase 'let firstName = 'M' '
@@ -27,3 +27,41 @@ Conventions for variable names
 - Descriptive names 'myFirstJob, myCurrentJob' instead of 'job2, job2'
 
 */
+
+//// DATA TYPES ////
+
+/* 
+
+Every value is an object or primitive type
+
+ 7 PRIMITIVE DATA TYPES
+ 1. Number: Floating point numbers, used for decimal or integers: let num = 3
+ 2. String: Sequence of characters, used for text: let name = 'Mell'
+ 3. Boolean: Logical type that can only be true or false, used for taking decisions: let fullAge = true
+ 4. Undefined: Values taken by a variable not yet defined (empty value): let children
+ 5. Null: Also means empty value
+ 6. Symbol: Value that is unique and cannot be changed 
+ 7. BigInt: Larger integers than the number type can hold
+
+*/
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log('Mell');
+
+// Reassign variable
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+// JS bug: returns object, but should return null
+console.log(typeof null);
