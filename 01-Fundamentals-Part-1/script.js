9ba508bcad5923ef54256ff4f2236d65207d6b0b
Mareike Heuer <mareike.heuer@hotmail.com>
2025-08-27 16:43:01 +0200

    Section 2: Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,6 +1,29 @@
 /* JS code in its own file --> Better separation of concern between presentation and logic */
 
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
 console.log(40 + 8 + 23 - 10);
+
+//// VALUES ////
+
+// Value is a piece of data, smallest unit of information
+console.log('Mell');
+console.log(23);
+
+// Declare a variable and store value inside. camelCase convention
+let firstName = 'Manon';
+let first = 'Mell';
+let firstNamePerson;
+
+console.log(firstName);
+console.log(firstName);
+
+/* 
+Conventions for variable names
+
+- camelCase 'let firstName = 'M' '
+- No special charactes 'let m&m = 3'
+- No numbers
+- Upper case ist for OOP 'let Person = 'M' '
+- Descriptive names 'myFirstJob, myCurrentJob' instead of 'job2, job2'
+
+*/
