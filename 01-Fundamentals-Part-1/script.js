3347e8e216930df2f3ceeecb37fc9a768f698fda
Mareike Heuer <mareike.heuer@hotmail.com>
2025-08-28 16:32:26 +0200

    Section 2: Operator Precedence

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -94,6 +94,8 @@ console.log(lastName);
 
 //// BASIC OPERATORS ////
 
+/*
+
 // AARITHMETIC OPERATORS
 const now = 2037;
 const ageMell = now - 1990;
@@ -126,3 +128,28 @@ const isFullAge = ageSarah >= 18;
 
 // Operation DOES NOT proceed from left to right
 console.log(now - 1990 > now - 2018);
+*/
+
+//// OPERATOR PRECEDENCE ////
+
+const now = 2037;
+const ageMell = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5; // Math operator (right to left) takes precedence over the equality operator
+console.log(x, y); // 10 10
+
+const averageAge = (ageMell + ageSarah) / 2; // Parenthesis gives precedence to addition
+console.log(ageMell, ageSarah, averageAge);
+
+/* 
+JS has a defined order or operator precedence, check table at MDN docs
+
+Math operators higher precedence than comparison operators 
+
+*/
