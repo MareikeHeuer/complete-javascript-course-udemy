b0725bddeccfb678738504cbbe0808eb29439aeb
Mareike Heuer <mareike.heuer@hotmail.com>
2025-08-28 18:04:01 +0200

    Section 2: Coding Challenge 2

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -224,6 +224,8 @@ If else statement is an if/else control structure, because this allows us to hav
 over the way the code is execuded.
 
 */
+
+/*
 const age = 15;
 
 if (age >= 18) {
@@ -242,3 +244,29 @@ if (birthYear <= 2000) {
   century = 21;
 }
 console.log(century);
+*/
+
+//// CODING CHALLENGE 2 🙂 ////
+
+/* 
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example:"Mark's BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement 😉 
+*/
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's! (${BMIJohn})`);
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's! (${BMIMark})`);
+}
