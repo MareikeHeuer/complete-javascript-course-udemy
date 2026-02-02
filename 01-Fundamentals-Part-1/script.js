2b6f2d8c29d8acb64cecfbf1d66375c901d5606b
Mareike Heuer <mareike.heuer@hotmail.com>
2026-02-02 16:17:36 +0100

    Section 2: The Switch Statement

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -461,6 +461,7 @@ TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110*/
 // }
 
 // Bonus 1, Bonus 2
+/*
 scoreDolphins = (97 + 112 + 101) / 3;
 scoreKoalas = (109 + 95 + 123) / 3;
 
@@ -476,4 +477,50 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
   console.log('Both win the trophy');
 } else {
   console.log('Noone wins the trophy');
+}
+  */
+
+//// THE SWITCH STATEMENT ////
+
+// Alternative way of writing a complicated if else statement when al we want to do is compare one value to multiple different options
+// Weekday variable with different acttivities, map one activity to each day
+
+const day = 'monday';
+
+switch (day) {
+  case 'monday':
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break; // without the break, code keeps executing
+  case 'tuesday':
+    console.log('Prepare thery videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+  case 'friday':
+    console.log('Record videos');
+    break;
+
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day!');
+}
+
+if (day === 'monday') {
+  console.log('Plan course structure');
+  console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+  console.log('Prepare thery videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+  console.log('Write code examples');
+} else if (day === 'friday') {
+  console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+  console.log('Enjoy the weekend :D');
+} else {
+  console.log('Not a valid day!');
 }
