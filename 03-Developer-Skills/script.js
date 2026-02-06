d0517ba43c4999ee075e75ddf6097f8d447c956a
Mareike Heuer <mareike.heuer@hotmail.com>
2026-02-06 16:08:58 +0100

    Section 5: Debugging (Fixing Errors)

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,24 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // C FIX BUG
+    value: Number(prompt('Degrees celsius')),
+  };
+
+  // B FIND BUG
+  console.log('Onject measureKelvin', measurement);
+  console.table(measurement);
+
+  console.log('measurement value', measurement.value);
+  //   console.warn('measurement value warn', measurement.value);
+  //   console.error('measurement value erorr', measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+// IDENTIFY BUG
+console.log(measureKelvin()); // enter 10, then 10273, prompt always retufns string
