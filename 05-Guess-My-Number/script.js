f621a846b591f8779abc62ee0e419602e7146d00
Mareike Heuer <mareike.heuer@hotmail.com>
2026-02-09 18:59:28 +0100

    Section 7: What is DOM and DOM Manipulation

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -2,3 +2,19 @@
 
 // querySelector() is a method that's available on the document
 console.log(document.querySelector('.message').textContent);
+
+/*
+WHAT IS DOM AND DOM MANIPULATION?
+
+- DOM: Document Object Model: Structured complete representation of HTML documents. 
+  Allows JS to access HTML element and styles to manipulate them.
+
+- DOM is automatically created by the browser as soon as the HTML document loads as a DOM tree
+
+- DOCUMENT: Special object that is the entry point to the DOM
+  EG: document.querySelector() 
+
+- DOM methods and properties are NOT part of JS , they are part of Web APIs
+- APIs are librearies also written in JS, and can interact with JS
+
+*/
