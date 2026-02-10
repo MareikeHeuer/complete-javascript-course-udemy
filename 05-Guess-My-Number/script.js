1c1d049ebe10939d126ca78402235a7f458e8761
Mareike Heuer <mareike.heuer@hotmail.com>
2026-02-10 20:30:55 +0100

    Section 7: Manipulating CSS Styles

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -44,6 +44,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number! ⛔️';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number! 🎉';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high 📈';
