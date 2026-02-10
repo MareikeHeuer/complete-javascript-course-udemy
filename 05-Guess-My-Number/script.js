2fea7ca92c7cec928d1f59ebcac837eca9cbf53f
Mareike Heuer <mareike.heuer@hotmail.com>
2026-02-10 20:46:58 +0100

    Section 7: Coding Challenge 1

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -30,8 +30,7 @@ console.log(document.querySelector('.guess').value);
 
 // Define secret number between 1 and 20 otuside of handler function, we need it once
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
-
+// console.log(secretNumber);
 let score = 20;
 
 // Handling click event with eventListener
@@ -39,11 +38,14 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  // Check if thee is a value
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number! ⛔️';
+
+    // Player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number! 🎉';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
@@ -55,7 +57,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game 💥';
       document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < secretNumber) {
+  } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high 📈';
       score--;
@@ -66,3 +68,14 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+// CODING CHALLENGE 1
+
+/*
+Implement a game rest functionality so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and number variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the oiginal backgroun color #222 and number width 15 rem
+ */
