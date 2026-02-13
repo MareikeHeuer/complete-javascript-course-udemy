9180125bce0602cfdbbd9710cf973520b9885d39
Mareike Heuer <mareike.heuer@hotmail.com>
2026-02-13 15:59:53 +0100

    Section 7: Implementing the Highscore

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -32,6 +32,7 @@ console.log(document.querySelector('.guess').value);
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 let score = 20;
+let highscore = 0;
 
 // Handling click event with eventListener
 document.querySelector('.check').addEventListener('click', function () {
@@ -48,6 +49,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // Number is too high
   } else if (guess > secretNumber) {
