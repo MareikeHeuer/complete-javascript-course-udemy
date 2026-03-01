ad2196bcafc04b62fd4b5b77277a5f7bef4e27cf
Mareike Heuer <mareike.heuer@hotmail.com>
2026-03-01 13:53:17 +0100

    Section 7: Project 3: Pig Game: Hide elements

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,2 +1,11 @@
-// Starter file Pig Game
 'use strict';
+
+// # selects by ID
+const score0El = document.querySelector('#score--0');
+// or use function to get the ID
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
