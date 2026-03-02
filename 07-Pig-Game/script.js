ea9b3664b4148f21544c83331b03d8c5b2dbfe8e
Mareike Heuer <mareike.heuer@hotmail.com>
2026-03-02 10:26:05 +0100

    Section 7: Project 3: Pig Game: Switching the Active Player

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 // # selects by ID
 const score0El = document.querySelector('#score--0');
 // or use function to get the ID
@@ -15,7 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling the dice functionality
 btnRoll.addEventListener('click', function () {
@@ -29,8 +33,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // change later
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    // Toggle will add class if it's not there and remove if it's there
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
